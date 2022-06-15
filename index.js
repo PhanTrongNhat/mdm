3b7e18eedf9c5811188dba5dab32d87868146164
PhanTrongNhat <48424808+PhanTrongNhat@users.noreply.github.com>
2022-06-15 23:09:19 +0700

    add mongooes

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import neo4j from "neo4j-driver";
+let driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("mdm", "123456")
+);
+const session = driver.session();
+const app = express();
+let port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cors());
+//   "mongodb+srv://udptbackend:udpt123456@cluster0.uohe5.mongodb.net/UDPT_backend?retryWrites=true&w=majority",
+mongoose.connect(
+  "mongodb+srv://nhat:123456nhat@mdm.l7t9g4e.mongodb.net/?retryWrites=true&w=majority",
+// "mongodb+srv://udptbackend:udpt123456@cluster0.uohe5.mongodb.net/UDPT_backend?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!!!");
+  }
+);
+
+app.get("/", async function (req,res) {
+
+  const num_nodes = await session.run("MATCH (n) RETURN n", {});
+  session.close();
+  console.log("RESULT", !num_nodes ? 0 : num_nodes.records.length);
+ res( !num_nodes ? 0 : num_nodes.records.length);
+});
+app.listen(port);
+
+console.log("RESTful API server started on: " + port);
