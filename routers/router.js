993afd52274bd08ceed6007fa0761d1d33f18acc
PhanTrongNhat <48424808+PhanTrongNhat@users.noreply.github.com>
2022-06-19 23:10:32 +0700

    add model tt xa

--- routers/router.js
+++ routers/router.js
@@ -4,13 +4,13 @@ import {
   getUser,
   registerUser,
   getAllTinh,
-  insertData,
+
 } from "../controllers/users.controller.js";
 const router = express.Router();
 
 router.get("/",getAll );
 router.get("/user",getUser);
-router.get("/insert",insertData);
+// router.get("/insert",insertData);
 router.get("/tatCaTinh", getAllTinh);
 router.post("/register", registerUser);
 export default router;
