34c92c6c159123fec2a812fd9224630c41ec05b9
HoangQuan0302 <quanminhaa@gmail.com>
2022-06-19 23:12:44 +0700

    Quan update register vaccin

--- controllers/registerVaccin.controller.js
+++ controllers/registerVaccin.controller.js
@@ -1,6 +1,18 @@
+import tinh from "../models/tinh.models.js";
+import huyen from "../models/huyen.model.js";
+import dbMongo from '../dbMongo.js';
+
 
 export const getPage = async (req, res) => {
-    res.render("RegisterVaccination");
+    try{
+        dbMongo._connect();
+        const Tinh= await tinh.find();
+        const Huyen=await huyen.find();
+        res.render("RegisterVaccination", {
+            tinh: Tinh,
+            huyen: Huyen,
+          });
+        } catch (error) {}
 };
 
 
